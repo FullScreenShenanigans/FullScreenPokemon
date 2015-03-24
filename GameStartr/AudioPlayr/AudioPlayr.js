e6a46c31ed6c2336f21b1ef27ccd588533049f5e
Joe Pringle <pringj3@rpi.edu>
2015-03-24 15:34:38 -0400

    add some style

--- GameStartr/AudioPlayr/AudioPlayr.js
+++ GameStartr/AudioPlayr/AudioPlayr.js
@@ -1,5 +1,11 @@
 /*
-    ROUGHLY WHAT WE NEED
+    AudioPlayr.js
+
+    @author "Joe Pringle"   <explodingp@gmail.com>
+    @author "Josh Goldberg" <josh@fullscreenmario.com>
+
+    
+    A few notes that'll get moved to the readme later on:
  
  library - store a listing of GBS files. Looks like we'll need at least 2. One for the themes, one for 
             various pokemon sounds and other misc sound effects.
@@ -7,19 +13,13 @@
             In audio.js the gbs data will be stored as a base64 encoded string. Later on, however, we'll
             decode that and ascii-fy each character to play nicely with the player. 
  
-            (TODO CORRECT JS SYNTAX LOL)
- 
-            something like:
-                    var payload = window.atob(settings.library["blueThemes"]["gbs"]).split('').map(
-                        function(c) {return c.charCodeAt(0);}) ;
- 
             then, audioPlayr can interpret 
  
                     play("ThemeViridianCity") 
  
             as something like
  
-                    play_music_data(payload, 10);
+                    play_music_data(decodedPayload, 10);
             
             which our music player understands!
  
@@ -36,12 +36,9 @@
                                 }
  
             Unfortunately, to save space, I don't think the theme names are included in the .gbs file, so
-            I'll scrape them from somewhere online and include them in audio.js
- 
- 
+            I'll scrape them from somewhere online and include them in audio.js.
  
  */
-
 function AudioPlayr(settings) {
     "use strict";
     if (!this || this === window) {
@@ -49,10 +46,10 @@ function AudioPlayr(settings) {
     }
     var self = this,
 
-        // A listing of GBS sources
+        // A listing of GBS sources.
         library,
 
-        // Master lookup table, keyed by trackName
+        // Master lookup table, keyed by trackName.
         directory,
 
         // The currently playing theme.
@@ -64,24 +61,33 @@ function AudioPlayr(settings) {
     /**
      * Resets the AudioPlayr.
 
+     @constructor
+     @param {Object} library     - Tracklists and encoded contents of any 
+                                    sound files, keyed by file.
+     @param {Object} statistics - Should contain "muted" and "volume" 
+                                    values to be passed to the internal StatsHolder.
      */
     self.reset = function (settings) {
+
+        if (typeof settings.library === "undefined") {
+            throw new Error("No library given to AudioPlayr.");
+        }
+
+        if (typeof settings.statistics === "undefined") {
+            throw new Error("No library given to AudioPlayr.");
+        }
+
         library = settings.library;
         StatsHolder = new StatsHoldr(settings.statistics);
 
+        // Initially, the directory is empty, and nothing is playing.
         directory = {};
         theme = null;
 
-        /*
-        @Josh - I should be able to do this here, not in play(), right?
-
-        declare variables to help libgme.js play our file via webkit audio api
-        var ref;
-        var emu;
-        var node;
-        */
-
+        // Decode and ascii-fy all "gbs" library entries.
         decodeAll();
+
+        // Create paths between trackName and actual playback information.
         populateDirectory();
     };
 
@@ -94,7 +100,6 @@ function AudioPlayr(settings) {
      */
     
     function decodeAll() {
-
         for (var i in library){
             library[i]["gbs"] = window.atob(library[i]["gbs"]).split('').map(
                 function(c) {
@@ -102,15 +107,13 @@ function AudioPlayr(settings) {
                     }
                 );
         }
-
     }
 
     /*
-    Once all "gbs" entries have been decoded, scan through the library and store
-    relevant playback information in the directory, keyed by trackName 
+    Once all "gbs" entries have been decoded, scan through the library and 
+    store relevant playback information in the directory, keyed by trackName .
     */
     function populateDirectory() {
-
         for (var i in library) {
             for (var track in library[i]["tracks"]) {
                 directory[track] = {
@@ -119,7 +122,6 @@ function AudioPlayr(settings) {
                 }
             }
         }
-
     }
 
     self.getLibrary = function () {
@@ -130,83 +132,84 @@ function AudioPlayr(settings) {
         return directory;
     }
 
-
-    /* Initialize ref, emu, node, ctx */
-    function setupAudioContext() {
-
-
-
-
-    }
     /* 
     Play a sound or theme, keyed by track name. 
-    FSP.AudioPlayer.play("openingTheme")
+    FSP.AudioPlayer.play("openingTheme");
     */
     self.play = function(track) {
-        // Grab necessary information to play the track from "library" and "directory"
         var payload = library[directory[track]["gbs_source"]]["gbs"];
         var subtune   = directory[track]["track_num"];
     
-        // required for libgme.js
+        // Required for libgme.js
         var ref = Module.allocate(1, "i32", Module.ALLOC_STATIC);
-        // FSP doesn't run on Safari anyway, so assume the user is opening with Chrome
-        var ctx = new AudioContext(); // | window.HTMLAudioElement
+        var ctx = new AudioContext(); 
 
-        // attempt to open the payload
-        if (Module.ccall("gme_open_data", "number", ["array", "number", "number", "number"], [payload, payload.length, ref, ctx.sampleRate]) != 0){
+        if (Module.ccall("gme_open_data", "number", 
+            ["array", "number", "number", "number"], 
+            [payload, payload.length, ref, ctx.sampleRate]) != 0){
             console.error("gme_open_data failed.");
             return;
         }
 
-        // determine the type of emulator to use to play this payload
+        // Determine the type of emulator to use to play this payload.
         var emu = Module.getValue(ref, "i32");
 
-        if (Module.ccall("gme_start_track", "number", ["number", "number"], [emu, subtune]) != 0)
+        if (Module.ccall("gme_start_track", "number", 
+            ["number", "number"], [emu, subtune]) != 0)
             console.log("could not load track");
 
-        // actually play the track
+        // Actually play the track.
         theme = track;
         var node = play_song(ctx, emu);
         window.savedReferences = [ctx, node];
 
     }
 
-    /* Private function that ACTUALLY plays the song, in user's current context */
+    /* 
+    Private function that ACTUALLY plays the song, in user's current context.
+     */
     function play_song (ctx, emu) {
         var node;
         var bufferSize = 1024 * 16;
         var inputs = 2;
         var outputs = 2;
         
-        if(!node && ctx.createJavaScriptNode)node = ctx.createJavaScriptNode(bufferSize, inputs, outputs);
-        if(!node && ctx.createScriptProcessor)node = ctx.createScriptProcessor(bufferSize, inputs, outputs);
+        if(!node && ctx.createJavaScriptNode)
+            node = ctx.createJavaScriptNode(bufferSize, inputs, outputs);
+        if(!node && ctx.createScriptProcessor)
+            node = ctx.createScriptProcessor(bufferSize, inputs, outputs);
 
         var buffer = Module.allocate(bufferSize * 2, "i32", Module.ALLOC_STATIC);
 
         var INT16_MAX = Math.pow(2, 32) - 1;
 
         node.onaudioprocess = function(e) {
-            if (Module.ccall("gme_track_ended", "number", ["number"], [emu]) == 1) {
-                // Can put any 'end-of-song' event handlers here, once audioPlayr is more fleshed out
+            if (Module.ccall("gme_track_ended", "number", ["number"],
+                     [emu]) == 1) {
+                // Can put any 'end-of-song' event handlers here, once audioPlayr 
+                // is more fleshed out
                 node.disconnect();
-                node = null;
-                
                 console.log("end of song");
                 theme = null;
                 return;
             }
 
-            var channels = [e.outputBuffer.getChannelData(0), e.outputBuffer.getChannelData(1)];
+            var channels = [e.outputBuffer.getChannelData(0), 
+                                e.outputBuffer.getChannelData(1)];
 
-            var err = Module.ccall("gme_play", "number", ["number", "number", "number"], [emu, bufferSize * 2, buffer]);
+            var err      = Module.ccall("gme_play", "number", 
+                            ["number", "number", "number"], 
+                            [emu, bufferSize * 2, buffer]);
 
             if (err) {
                 console.log("error with Module.gme_play");
             }
 
             for (var i = 0; i < bufferSize; i++)
-                for (var n = 0; n < e.outputBuffer.numberOfChannels; n++)
-                    channels[n][i] = Module.getValue(buffer + i * e.outputBuffer.numberOfChannels * 2 + n * 4, "i32") / INT16_MAX;
+                for (var n = 0; n < e.outputBuffer.numberOfChannels; n++) {
+                    var tmp        = buffer + i * e.outputBuffer.numberOfChannels * 2 + n * 4;
+                    channels[n][i] = Module.getValue(tmp, "i32") / INT16_MAX;
+                }
         } 
 
         node.connect(ctx.destination)
