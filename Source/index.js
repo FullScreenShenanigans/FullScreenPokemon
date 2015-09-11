a735fe5feb677912a09e352e63e645c579608299
Josh Goldberg <josh@fullscreenmario.com>
2015-09-10 19:45:16 -0700

    Re-added .gitignore, Source\inde.xjs

---
+++ Source/index.js
@@ -0,0 +1,14 @@
+﻿var time = Date.now();
+
+document.onreadystatechange = function (event) {
+    if (event.target.readyState !== "complete") {
+        return;
+    }
+
+    var UserWrapper = new UserWrappr.UserWrappr(FullScreenPokemon.FullScreenPokemon.prototype.proliferate(
+        {
+            "GameStartrConstructor": FullScreenPokemon.FullScreenPokemon
+        }, FullScreenPokemon.FullScreenPokemon.settings.ui, true));
+
+    console.log("It took " + (Date.now() - time) + " milliseconds to start."), UserWrapper.displayHelpMenu()
+};
