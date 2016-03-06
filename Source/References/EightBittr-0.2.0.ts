cc062cfaedaf1245b38972124f4957bbd1ab9f5c
Blake Lingenau <blake.lingenau@gmail.com>
2016-03-06 13:31:25 -0500

    Fixing an error in documentation

--- Source/References/EightBittr-0.2.0.ts
+++ Source/References/EightBittr-0.2.0.ts
@@ -626,7 +626,7 @@ module EightBittr {
         }
 
         /**
-         * Shifts a Thing horizontally by changing its top and bottom attributes.
+         * Shifts a Thing horizontally by changing its left and right attributes.
          * 
          * @param thing   The Thing to shift horizontally.
          * @param dy   How far to shift the Thing.
