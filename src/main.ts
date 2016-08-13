3287607363356978591b8e4a0a3cdf94071e6de4
Josh Goldberg <josh@fullscreenmario.com>
2016-08-13 16:37:32 -0700

    Added test logs to main.ts

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,8 @@ if (typeof require === "undefined") {
     new UserWrappr.UserWrappr((window as any).FullScreenPokemon.FullScreenPokemon.prototype.settings.ui);
 } else {
     require(["FullScreenPokemon"], (FSP: any): void => {
+        console.log("FSP", FSP);
+        console.log("window", window);
         new UserWrappr.UserWrappr(FSP.FullScreenPokemon.prototype.settings.ui);
     });
 }
