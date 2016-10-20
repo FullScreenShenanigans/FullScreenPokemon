8a94a45aed30973c2dbfcbc545f2d6b09440c535
Blake Lingenau <blake.lingenau@gmail.com>
2016-10-20 16:14:20 -0400

    Added mod for "Mobile PokeCenter'. Fixes #241

--- src/Settings/Mods.ts
+++ src/Settings/Mods.ts
@@ -316,6 +316,28 @@ export function GenerateModsSettings(): GameStartr.IModAttachrCustoms {
                         }
                     }
                 }
+            },
+            {
+                name: "Mobile PokeCenter",
+                enabled: false,
+                events: {
+                    onModEnable: function (mod: ModAttachr.IMod, eventName: string, args: any[]): void {
+                        const party: IPokemon[] = this.ItemsHolder.getItem("PokemonInParty");
+                        const onChange: (enabled: boolean) => void = args[1][0];
+
+                        for (const pokemon of party) {
+                            this.battles.healPokemon(pokemon);
+                        }
+
+                        this.menus.displayMessage("Your party has been fully healed!");
+                        this.ModAttacher.toggleMod("Mobile PokeCenter");
+                        // Call onChange with true because UserWrappr flips it right after.
+                        onChange(true);
+                    },
+                    onModDisable: function (mod: ModAttachr.IMod): void {
+                        return;
+                    }
+                }
             }
         ]
     };
