18a8fee65cda7aa2098dac02ae204721a6180916
Josh Goldberg <josh@fullscreenmario.com>
2016-12-02 18:47:31 -0800

    Added dependencies code for index.html

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,7 @@ import { UserWrappr } from "userwrappr/lib/UserWrappr";
 import { FullScreenPokemon } from "./FullScreenPokemon";
 
 ((): void => {
-    const onLoad: () => void = (): void => {
-        (window as any).UserWrapper = new UserWrappr(FullScreenPokemon.prototype.moduleSettings.ui);
-        window.removeEventListener("load", onLoad);
-    };
-
-    window.addEventListener("load", onLoad);
+    /* tslint:disable */
+    new UserWrappr(FullScreenPokemon.prototype.moduleSettings.ui);
+    /* tslint:enable */
 })();
--- test/FullScreenPokemon/_.ts
+++ test/FullScreenPokemon/_.ts
@@ -1,3 +1,15 @@
-import { mochaLoader } from "../main";
+import { IGameStartrSettings } from "gamestartr/lib/IGameStartr";
 
-mochaLoader.it("_", (): void => { });
+import { mochaLoader } from "../main";
+import { FullScreenPokemon } from "../../src/FullScreenPokemon";
+
+mochaLoader.it("can be created", (): void => {
+    // Arrange
+    const settings: IGameStartrSettings = {
+        width: 256,
+        height: 256
+    };
+
+    // Act
+    chai.expect(() => new FullScreenPokemon(settings)).to.not.throw;
+});
