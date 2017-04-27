e5d33065579d1faa7e4712e936fa521c6da82267
Connor Hadley <Connor Hadley>
2017-04-27 15:19:16 -0400

    Made unit tests more self-contained

--- test/components/isEvolved.ts
+++ test/components/isEvolved.ts
@@ -1,56 +1,56 @@
 import { IPokemon } from "../../src/components/Battles";
-import { IPokemonEvolution, IPokemonEvolutionByLevel } from "../../src/components/constants/Pokemon";
+import { IPokemonEvolutionByLevel } from "../../src/components/constants/Pokemon";
 import { FullScreenPokemon } from "../../src/FullScreenPokemon";
 import { it } from "../main";
 import { stubBlankGame } from "../utils/fakes";
 
-it("properly evolves a Pokemon at exactly its level requirement", (): void => {
+it("evolves a Pokemon at exactly its level requirement", (): void => {
     // Arrange
     const fsp: FullScreenPokemon = stubBlankGame();
     const pokemonTitle: string[] = "CHARMANDER".split("");
-    const evolutions: IPokemonEvolution[] = fsp.constants.pokemon.byName[pokemonTitle.join("")].evolutions!;
-    const pokemonLevel: number = (evolutions[0].requirements[0] as IPokemonEvolutionByLevel).level - 1;
+    const pokemonEvolution: IPokemonEvolutionByLevel = {
+        "method": "level",
+        "level": 16
+    };
 
     // Act
-    const pokemon: IPokemon = fsp.equations.newPokemon(pokemonTitle, pokemonLevel);
+    const pokemon: IPokemon = fsp.equations.newPokemon(pokemonTitle, pokemonEvolution.level - 1);
     fsp.experience.levelup(pokemon);
 
     // Assert
-    chai.expect(pokemon.title.toString()).to.be.equal(
-        "CHARMELEON".split("").toString()
-    );
+    chai.expect(pokemon.title.toString()).to.be.equal("CHARMELEON".split("").toString());
 });
 
-it("properly evolves a Pokemon that exceeds its level requirement", (): void => {
+it("evolves a Pokemon that exceeds its level requirement", (): void => {
     // Arrange
     const fsp: FullScreenPokemon = stubBlankGame();
     const pokemonTitle: string[] = "CHARMANDER".split("");
-    const evolutions: IPokemonEvolution[] = fsp.constants.pokemon.byName[pokemonTitle.join("")].evolutions!;
-    const pokemonLevel: number = (evolutions[0].requirements[0] as IPokemonEvolutionByLevel).level + 1;
+    const pokemonEvolution: IPokemonEvolutionByLevel = {
+        "method": "level",
+        "level": 16
+    };
 
     // Act
-    const pokemon: IPokemon = fsp.equations.newPokemon(pokemonTitle, pokemonLevel);
+    const pokemon: IPokemon = fsp.equations.newPokemon(pokemonTitle, pokemonEvolution.level + 1);
     fsp.experience.levelup(pokemon);
 
     // Assert
-    chai.expect(pokemon.title.toString()).to.be.equal(
-        "CHARMELEON".split("").toString()
-    );
+    chai.expect(pokemon.title.toString()).to.be.equal("CHARMELEON".split("").toString());
 });
 
 it("does not evolve a Pokemon that has not yet reached its level requirement", (): void => {
     // Arrange
     const fsp: FullScreenPokemon = stubBlankGame();
     const pokemonTitle: string[] = "CHARMANDER".split("");
-    const evolutions: IPokemonEvolution[] = fsp.constants.pokemon.byName[pokemonTitle.join("")].evolutions!;
-    const pokemonLevel: number = (evolutions[0].requirements[0] as IPokemonEvolutionByLevel).level - 2;
+    const pokemonEvolution: IPokemonEvolutionByLevel = {
+        "method": "level",
+        "level": 16
+    };
 
     // Act
-    const pokemon: IPokemon = fsp.equations.newPokemon(pokemonTitle, pokemonLevel);
+    const pokemon: IPokemon = fsp.equations.newPokemon(pokemonTitle, pokemonEvolution.level - 2);
     fsp.experience.levelup(pokemon);
 
     // Assert
-    chai.expect(pokemon.title.toString()).to.be.equal(
-        "CHARMANDER".split("").toString()
-    );
+    chai.expect(pokemon.title.toString()).to.be.equal("CHARMANDER".split("").toString());
 });
