93e65cad2cb63a26b6c3132b453d2eb83d182c4b
chrisNvalmas <christophervalmas@gmail.com>
2017-06-02 01:57:38 -0400

    Updated with asked for changes#2

--- src/components/mods/RandomizeWildPokemonMod.ts
+++ src/components/mods/RandomizeWildPokemonMod.ts
@@ -19,26 +19,18 @@ export class RandomizeWildPokemonMod<TGameStartr extends FullScreenPokemon> exte
     public readonly events: ICallbackRegister = {
         onRandomizePokemon: (chosen: IWildPokemonSchema): IWildPokemonSchema => {
             const pokemonName: string = chosen.title.join("");
-            const pokemonTypes: string[] = [];
-            for (const type of this.gameStarter.constants.pokemon.byName[pokemonName].types) {
-                pokemonTypes.push(type);
-            }
-
+            const pokemonTypes: string[] = this.gameStarter.constants.pokemon.byName[pokemonName].types;
             const randomPokemon: string[] = [];
             for (const pokemon in this.gameStarter.constants.pokemon.byName) {
                 for (const randomsType of this.gameStarter.constants.pokemon.byName[pokemon].types) {
                     if (pokemonTypes.indexOf(randomsType) !== -1) {
                         randomPokemon.push(pokemon);
                     }
-
                 }
-
             }
 
             chosen.title = this.gameStarter.numberMaker.randomArrayMember(randomPokemon).split("");
             return chosen;
         }
-
-    };
-    
+    };    
 } 
