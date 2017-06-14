37942e7ce3bd54324de6b9b7b247ccdce6968a3a
chrisNvalmas <christophervalmas@gmail.com>
2017-06-14 11:15:46 -0400

    Updated tests

--- test/components/canBeCreatedWithItems.ts
+++ test/components/canBeCreatedWithItems.ts
@@ -4,12 +4,11 @@ import { FullScreenPokemon } from "../../src/FullScreenPokemon";
 import { it } from "../main";
 import { stubBlankGame } from "../utils/fakes";
 
-const fsp: FullScreenPokemon = stubBlankGame();
 const pokemonTitle: string[] = "CHARMANDER".split("");
 
 it("Pokemon can be given an item when created", (): void => {
     // Arrange
-
+    const fsp: FullScreenPokemon = stubBlankGame();
     // Act
     const pokemon: IPokemon = fsp.equations.newPokemon({
         level: 1,
@@ -22,10 +21,10 @@ it("Pokemon can be given an item when created", (): void => {
     chai.expect(pokemon.item).to.deep.equal("Potion".split(""));
 });
 
-fsp.modAttacher.enableMod("Random Held Items");
-
-it("Pokemon spawns with random item when the generated probability is valid", (): void => {
+it("RandomHeldItemMod makes Pokemon spawn with random item when the generated probability is valid", (): void => {
     // Arrange
+    const fsp: FullScreenPokemon = stubBlankGame();
+    fsp.modAttacher.enableMod("Random Held Items");
     fsp.numberMaker.randomReal1 = (): number => .012;
     const chosenInfo: INewPokemon = {
         level: 1,
@@ -39,10 +38,10 @@ it("Pokemon spawns with random item when the generated probability is valid", ()
     chai.expect(chosenPokemon.item).to.be.deep.equal("Burn Heal".split(""));
 });
 
-delete fsp.numberMaker.randomReal1;
-
-it("Pokemon spawns with no item when the generated probability is invalid", (): void => {
+it("RandomHeldItemMod makes Pokemon spawn with no item when the generated probability is invalid", (): void => {
     // Arrange
+    const fsp: FullScreenPokemon = stubBlankGame();
+    fsp.modAttacher.enableMod("Random Held Items");
     fsp.numberMaker.randomReal1 = (): number => 1.15;
     const chosenInfo: INewPokemon = {
         level: 1,
@@ -55,6 +54,3 @@ it("Pokemon spawns with no item when the generated probability is invalid", (): 
     // Assert
     chai.expect(chosenPokemon.item).to.be.deep.equal(undefined);
 });
-
-fsp.modAttacher.disableMod("Random Held Items");
-delete fsp.numberMaker.randomReal1;
