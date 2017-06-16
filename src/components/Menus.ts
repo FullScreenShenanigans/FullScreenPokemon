a87fcc96c984ebde6a921b7830e805f1a2dee828
chrisNvalmas <christophervalmas@gmail.com>
2017-06-16 16:03:38 -0400

    Fixed Pokedex menu height (#507)
    * Changed height of pokedex menu
    
    * Update Menus.ts

--- src/components/Menus.ts
+++ src/components/Menus.ts
@@ -187,7 +187,7 @@ export class Menus<TGameStartr extends FullScreenPokemon> extends Component<TGam
 
             this.gameStarter.menuGrapher.createMenu("Pause", {
                 size: {
-                    height: 64
+                    height: 256
                 }
             });
         } else {
