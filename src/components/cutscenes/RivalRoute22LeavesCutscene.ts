aee76a08447df67d8f0836155124a169ba948e79
chrisNvalmas <christophervalmas@gmail.com>
2017-06-30 00:38:50 -0400

    Create RivalRoute22LeavesCutscene.ts

---
+++ src/components/cutscenes/RivalRoute22LeavesCutscene.ts
@@ -0,0 +1,69 @@
+import { Component } from "eightbittr/lib/Component";
+
+import { FullScreenPokemon } from "../../FullScreenPokemon";
+import { IWalkingInstructions } from "../actions/Walking";
+import { Direction } from "../Constants";
+import { ICharacter } from "../Things";
+
+/**
+ * OakIntroRivalLeaves cutscene functions used by FullScreenPokemon instances.
+ */
+export class RivalRoute22LeavesCutscene<TGameStartr extends FullScreenPokemon> extends Component<TGameStartr> {
+    /**
+     * Cutscene for showing the lab after the battle ends.
+     */
+    public AfterBattle(): void {
+        this.gameStarter.mapScreener.blockInputs = true;
+        this.gameStarter.timeHandler.addEvent(this.gameStarter.scenePlayer.bindRoutine("Walking"), 49);
+    }
+
+    /**
+     * Cutscene for the rival leaving the lab and Oak giving the player advice.
+     */
+    public Walking(): void {
+        const rival: ICharacter = this.gameStarter.utilities.getThingById("Rival") as ICharacter;
+        const walkingInstructions: IWalkingInstructions = [
+            {
+                blocks: 1,
+                direction: Direction.Top
+            },
+            {
+                blocks: 3,
+                direction: Direction.Right
+            },
+            {
+                blocks: 8,
+                direction: Direction.Bottom
+            },
+            {
+                blocks: 6,
+                direction: Direction.Right
+            },
+            {
+                blocks: 3,
+                direction: Direction.Top
+            },
+            {
+                blocks: 19,
+                direction: Direction.Right
+            },
+            {
+                blocks: 1,
+                direction: Direction.Top
+            },
+            {
+                blocks: 5,
+                direction: Direction.Right
+            },
+            (): void => {
+                this.gameStarter.physics.killNormal(rival);
+                this.gameStarter.stateHolder.addChange(rival.id, "alive", false);
+                this.gameStarter.mapScreener.blockInputs = false;
+            }
+        ];
+
+        rival.nocollide = true;
+        this.gameStarter.actions.walking.startWalkingOnPath(rival, walkingInstructions);
+        this.gameStarter.scenePlayer.stopCutscene();
+    }
+}
