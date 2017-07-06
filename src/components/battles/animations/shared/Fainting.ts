68d567fc6689dfbc1d7353b76e0a35f9e2027e95
chrisNvalmas <christophervalmas@gmail.com>
2017-07-06 17:33:57 -0400

    Update Fainting.ts

--- src/components/battles/animations/shared/Fainting.ts
+++ src/components/battles/animations/shared/Fainting.ts
@@ -3,6 +3,7 @@ import { Component } from "eightbittr/lib/Component";
 
 import { FullScreenPokemon } from "../../../../FullScreenPokemon";
 import { IBattleInfo, IPokemon } from "../../../Battles";
+import { IExperienceGains } from "../../../Experience";
 import { IThing } from "../../../Things";
 
 /**
@@ -29,12 +30,7 @@ export class Fainting<TGameStartr extends FullScreenPokemon> extends Component<T
 
         this.gameStarter.battles.decorations.moveToBeforeBackground(blank);
         this.gameStarter.battles.decorations.moveToBeforeBackground(thing);
-        const experienceGained: number | void = this.isBattleAgainstTrainer(teamName, battleInfo.teams.opponent.leader, pokemon);
-        let experienceGainedString: string = "";
-
-        if (experienceGained !== undefined) {
-                experienceGainedString = String(experienceGained);
-        }
+        const experienceGained: IExperienceGains[] = this.isBattleAgainstTrainer(teamName, battleInfo.teams.opponent.leader, pokemon);
 
         this.gameStarter.physics.setLeft(blank, thing.left);
         this.gameStarter.physics.setTop(blank, thing.top + thing.height * thing.scale!);
@@ -56,10 +52,13 @@ export class Fainting<TGameStartr extends FullScreenPokemon> extends Component<T
                         [playerName, " is out of useable Pokemon!"],
                         [playerName, " blacked out!"]);
                 } else if (teamName === "opponent") {
-                    partyIsWipedText.push(
-                        [this.gameStarter.battleMover.getBattleInfo().teams.player.selectedActor.title, " gained ",
-                         experienceGainedString, " EXP. Points!"]
-                    );
+                    for (const pokemonExperience of experienceGained) {
+                        partyIsWipedText.push(
+                            [pokemonExperience.pokemonName, " gained ",
+                             pokemonExperience.experienceGained, " EXP. Points!"]
+                        );
+                    }
+                    this.OpponentFainted();
                 }
 
                 this.gameStarter.menuGrapher.addMenuDialog("GeneralText", partyIsWipedText, onComplete);
@@ -76,13 +75,13 @@ export class Fainting<TGameStartr extends FullScreenPokemon> extends Component<T
      * @param battleInfo    Used to tell if player is fighting a wild Pokemon or trainer.
      * @param pokemon   Information of defeated Pokemon.
      */
-    private isBattleAgainstTrainer(teamName: string, battleInfo: ITeamLeader | undefined , pokemon: IPokemon): number | void {
+    private isBattleAgainstTrainer(teamName: string, battleInfo: ITeamLeader | undefined , pokemon: IPokemon): IExperienceGains[] {
         if (teamName === "opponent") {
-            this.OpponentFainted();
             return battleInfo !== undefined
                 ? this.gameStarter.experience.calculateExperience(pokemon, true)
                 : this.gameStarter.experience.calculateExperience(pokemon, false);
         }
+        return [];
     }
 
     /**
