dca0b769ae30fe228f7b59f1278735f989a774fb
Machinocheese <zbill15@yahoo.com>
2018-03-11 13:05:47 -0400

    stylistic changes

--- src/components/MoveAdder.ts
+++ src/components/MoveAdder.ts
@@ -6,8 +6,14 @@ import { IPokemon } from "./Battles";
 
 export class MoveAdder<TGameStartr extends FullScreenPokemon> extends GeneralComponent<TGameStartr> {
 
-    //returns whether the move has been successfully added or not
-    public AddMove(pokemon: IPokemon, move: IMove, index: number) {
+    /**
+     * Adds a new move to a pokemon's set of moves.
+     *
+     * @param pokemon The pokemon whose moveset is being modified.
+     * @param move The move that's going to be added into the moveset.
+     * @param index The position the move is going into in the IMove[] array.
+     */
+    public addMove(pokemon: IPokemon, move: IMove, index: number) {
         pokemon.moves[index] = move;
     }
 }
