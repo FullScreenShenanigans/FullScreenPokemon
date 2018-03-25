8819d4c7cd6fd5b29e739f2c9ae1ce34cddc7fbc
Billy <zbill15@yahoo.com>
2018-03-25 16:07:35 -0400

    Fixed some style issues

--- src/components/MoveAdder.test.ts
+++ src/components/MoveAdder.test.ts
@@ -5,14 +5,13 @@ import { IPokemon } from "./Battles";
 import { MoveAdder } from "./MoveAdder";
 
 describe("MoveAdder", () => {
-    it("test Move Adder API", (): void => {
+    it("adds a new move to a Pokemon's moveset", (): void => {
         //Arrange
         const { fsp } = stubBlankGame();
         const pokemon: IPokemon = fsp.equations.newPokemon({
             level: 5,
             title: "SQUIRTLE".split(""),
         });
-        const adder = new MoveAdder(fsp);
         const peck: IMove = {
             title: "Peck",
             remaining: 10,
@@ -20,48 +19,59 @@ describe("MoveAdder", () => {
         };
 
         //Act
-        adder.addMove(pokemon, peck, 1);
+        fsp.moveadder.addMove(pokemon, peck, 1);
 
         //Assert
         expect(pokemon.moves[1].title === peck.title);
     });
 
-    it("test Move Adder API - edge cases", (): void => {
+    it("attempts adding a move at a negative index to a Pokemon", (): void => {
         //Arrange
         const { fsp } = stubBlankGame();
         const pokemon: IPokemon = fsp.equations.newPokemon({
             level: 5,
             title: "SQUIRTLE".split(""),
         });
-        const adder = new MoveAdder(fsp);
         const peck: IMove = {
             title: "Peck",
             remaining: 10,
             uses: 10,
         };
-        const bite: IMove = {
-            title: "Bite",
-            remaining: 10,
-            uses: 10,
-        };
 
         //Act
-        adder.addMove(pokemon, peck, -1);
-        adder.addMove(pokemon, bite, 4);
+        fsp.moveadder.addMove(pokemon, peck, -1);
 
         //Assert
         expect(pokemon.moves[-1].title !== peck.title);
-        expect(pokemon.moves[4].title !== bite.title);
     });
 
-    it("test Move Adder API - duplicate move", (): void => {
+    it("attempts adding more than 4 moves to a Pokemon", (): void => {
         //Arrange
         const { fsp } = stubBlankGame();
         const pokemon: IPokemon = fsp.equations.newPokemon({
             level: 5,
             title: "SQUIRTLE".split(""),
         });
-        const adder = new MoveAdder(fsp);
+        const peck: IMove = {
+            title: "Peck",
+            remaining: 10,
+            uses: 10,
+        };
+
+        //Act
+        fsp.moveadder.addMove(pokemon, peck, 4);
+
+        //Assert
+        expect(pokemon.moves[4].title !== peck.title);
+    });
+
+    it("attempts to add a move a Pokemon already knows", (): void => {
+        //Arrange
+        const { fsp } = stubBlankGame();
+        const pokemon: IPokemon = fsp.equations.newPokemon({
+            level: 5,
+            title: "SQUIRTLE".split(""),
+        });
         const peck: IMove = {
             title: "Peck",
             remaining: 10,
@@ -74,9 +84,9 @@ describe("MoveAdder", () => {
         };
 
         //Act
-        adder.addMove(pokemon, peck, 1);
-        adder.addMove(pokemon, bite, 2);
-        adder.addMove(pokemon, peck, 2);
+        fsp.moveadder.addMove(pokemon, peck, 1);
+        fsp.moveadder.addMove(pokemon, bite, 2);
+        fsp.moveadder.addMove(pokemon, peck, 2);
 
         //Assert
         expect(pokemon.moves[2].title === bite.title);
