22c45451c6de3ccf3a750315c477e5254a957370
Machinocheese <zbill15@yahoo.com>
2018-03-31 19:25:42 -0400

    Added logic for adding battle stats (#656)
    * Added logic for adding battle stats - dialog WIP
    
    * Added logic for X Attack/Defend/Special/Speed
    
    * Removed an accidental log/added some docs
    
    * Added revive functionality

--- src/components/equations/ItemEffects.ts
+++ src/components/equations/ItemEffects.ts
@@ -1,4 +1,4 @@
-import { IMove } from "battlemovr";
+import { IMove, IStatistic } from "battlemovr";
 import { GeneralComponent } from "gamestartr";
 
 import { FullScreenPokemon } from "../../FullScreenPokemon";
@@ -13,9 +13,89 @@ export class ItemEffects<TGameStartr extends FullScreenPokemon> extends GeneralC
      */
     public addPP(move: IMove, amount: number) {
         if (amount < 0) {
-            throw new Error("PP decrements aren't allowed");
+            throw new Error("PP decrements aren't allowed.");
         }
 
         move.remaining = Math.min(move.remaining + amount, move.uses);
+    }
+
+    /**
+     * Increases a stat by 1.
+     *
+     * @param statistic   The statistic that is being modified.
+     */
+    private increaseBattleStat(statistic: IStatistic) {
+        statistic.current = Math.min(statistic.normal * 4, (statistic.normal / 2) + statistic.current);
+    }
+
+    /**
+     * Opens dialog menu for using a battle item like X Attack / X Defend.
+     *
+     * @param pokemon   The pokemon who the X Item is being used on.
+     * @param statistic   The stat that is being modified by the item.
+     * @param type   The string representing the stat name.
+     */
+    public useXItem(pokemon: IPokemon, statistic: IStatistic, type: string) {
+
+        //TODO: Implement X Accuracy
+        //https://github.com/FullScreenShenanigans/FullScreenPokemon/issues/657
+
+        this.gameStarter.menuGrapher.createMenu("GeneralText", {
+            deleteOnFinish: true,
+        });
+        this.gameStarter.menuGrapher.addMenuDialog(
+            "GeneralText",
+            [
+                [
+                    "%%%%%%%PLAYER%%%%%%% used X " + type.toUpperCase() + "!",
+                ],
+                pokemon.title.join("") + "'s " + type.toUpperCase() + " rose!",
+            ],
+            (): void => {
+                this.increaseBattleStat(statistic);
+            });
+        this.gameStarter.menuGrapher.setActiveMenu("GeneralText");
+    }
+
+    /**
+     * Revives a Pokemon.
+     *
+     * @param pokemon   An in-game Pokemon to revive.
+     * @param percentage   The percentage of health restored by the revive (use 50% for Revive, 100% for Max Revive).
+     */
+    private revivePokemon(pokemon: IPokemon, percentage: number): void {
+        if (percentage < 0 || percentage > 1) {
+            throw new Error("Invalid percentage value.");
+        }
+
+        for (const statisticName of this.gameStarter.constants.pokemon.statisticNames) {
+            pokemon.statistics[statisticName].current = (statisticName === "health") ? pokemon.statistics[statisticName].normal * percentage
+                : pokemon.statistics[statisticName].current = pokemon.statistics[statisticName].normal;
+        }
+
+        pokemon.status = undefined;
+    }
+
+    /**
+     * The dialog options for using a Revive or Max Revive.
+     *
+     * @param pokemon   An in-game Pokemon to revive.
+     * @param percentage   The percentage of health restored by the revive (use 50% for Revive, 100% for Max Revive).
+     */
+    public useRevive(pokemon: IPokemon, percentage: number) {
+        this.gameStarter.menuGrapher.createMenu("GeneralText", {
+            deleteOnFinish: true,
+        });
+        this.gameStarter.menuGrapher.addMenuDialog(
+            "GeneralText",
+            [
+                [
+                    pokemon.title.join("") + " is revitalized!",
+                ],
+            ],
+            (): void => {
+                this.revivePokemon(pokemon, percentage);
+            });
+        this.gameStarter.menuGrapher.setActiveMenu("GeneralText");
     }
 }
