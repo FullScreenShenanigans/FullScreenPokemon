f06ade034e4325f9722221be355d4ec267871d4c
Billy <zbill15@yahoo.com>
2018-04-27 22:40:22 -0400

    Add new Pokemon

---
+++ src/components/equations/ItemEffects.test.ts
@@ -0,0 +1,51 @@
+import { expect } from "chai";
+import { stubBlankGame } from "../../fakes.test";
+import { IPokemon } from "../Battles";
+import { ItemEffects } from "./ItemEffects";
+
+describe("ItemEffects", () => {
+    describe("capturePokemon", () => {
+        it("adds a new Pokemon to your party when your party has fewer than six Pokemon", (): void => {
+            // Arrange
+            const { fsp } = stubBlankGame();
+            const pokemon: IPokemon = fsp.equations.newPokemon({
+                level: 5,
+                title: "SQUIRTLE".split(""),
+            });
+            const itemEffects = new ItemEffects(fsp);
+
+            // Act
+            itemEffects.capturePokemon(pokemon);
+            itemEffects.capturePokemon(pokemon);
+
+            // Assert
+            expect(fsp.itemsHolder.getItem(fsp.storage.names.pokemonInParty)[0].title).to.be.equal(pokemon.title);
+            expect(fsp.itemsHolder.getItem(fsp.storage.names.pokemonInParty)[1].title).to.be.equal(pokemon.title);
+        });
+
+        it("does not add a new Pokemon to your party when your party has more than six Pokemon", (): void => {
+            // Arrange
+            const { fsp } = stubBlankGame();
+            const squirtle: IPokemon = fsp.equations.newPokemon({
+                level: 5,
+                title: "SQUIRTLE".split(""),
+            });
+            const charmander: IPokemon = fsp.equations.newPokemon({
+                level: 5,
+                title: "CHARMANDER".split(""),
+            });
+            const itemEffects = new ItemEffects(fsp);
+            for (let i = 0; i < 6; i++) {
+                fsp.itemsHolder.getItem(fsp.storage.names.pokemonInParty)[i] = squirtle;
+            }
+
+            // Act
+            itemEffects.capturePokemon(charmander);
+
+            // Assert
+            for (let i = 0; i < 6; i++) {
+                expect(fsp.itemsHolder.getItem(fsp.storage.names.pokemonInParty)[i].title).to.be.equal(squirtle.title);
+            }
+        });
+     });
+});
