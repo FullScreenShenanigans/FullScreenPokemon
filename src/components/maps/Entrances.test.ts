2487dbff33a32608205f0edef8137de48eb106ae
Josh Goldberg <josh@fullscreenmario.com>
2019-03-18 16:14:29 -0400

    Ensured player position is always saved when resuming a game
    Fixes #727.

---
+++ src/components/maps/Entrances.test.ts
@@ -0,0 +1,43 @@
+import { expect } from "chai";
+
+import { stubBlankGame } from "../../fakes.test";
+
+describe("Entrances", () => {
+    describe("resume", () => {
+        it("defaults player position to (0, 0) when not previously saved", () => {
+            // Arrange
+            const { fsp } = stubBlankGame();
+            const zeroCorner = fsp.things.add(fsp.things.names.grass, 0, 0);
+
+            // Act
+            fsp.maps.entrances.resume();
+
+            // Assert
+            expect(fsp.groupHolder.getThing("player")).to.deep.include({
+                left: zeroCorner.left,
+                top: zeroCorner.top,
+            });
+        });
+
+        it("restores player position to the saved value when previously saved", () => {
+            // Arrange
+            const xloc = 40;
+            const yloc = 24;
+            const { fsp } = stubBlankGame();
+            const zeroCorner = fsp.things.add(fsp.things.names.grass, 0, 0);
+
+            fsp.stateHolder.addChange("player", "xloc", xloc);
+            fsp.stateHolder.addChange("player", "yloc", yloc);
+
+            // Act
+            document.body.appendChild(fsp.canvas);
+            fsp.maps.entrances.resume();
+
+            // Assert
+            expect(fsp.groupHolder.getThing("player")).to.deep.include({
+                left: zeroCorner.left + xloc,
+                top: zeroCorner.top + yloc,
+            });
+        });
+    });
+});
